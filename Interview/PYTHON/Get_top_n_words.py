ef1d61ee7784c45794d9498fdc31e580c13aba52
mashagua <993498039@qq.com>
2020-03-05 09:00:02 +0800

    Initial commit

---
+++ Interview/PYTHON/Get_top_n_words.py
@@ -0,0 +1,17 @@
+"""
+n = 3
+posting = "
+Herbal sauna uses the healing properties of herbs in combination with distilled water.
+The water evaporates and distributes the effect of the herbs throughout the room.
+A visit to the herbal sauna can cause real miracles, especially for colds.
+"
+
+output = [
+    ('the', 6),
+    ('herbal', 2),
+    ('sauna', 2),
+]
+
+"""
+def top_n(posting,n):
+    posting.split(" ")
